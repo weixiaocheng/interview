caa01ff31a88bc23472860502011c889f2aa5847
liuliang <454493675@qq.com>
2023-02-02 22:12:02 +0800

    添加项目初始化

---
+++ README.md
@@ -0,0 +1,11 @@
+### 面试录APP 
+
+### 需要完成的功能 
+1. 闪屏页面
+2. 实现tabbar
+   1. viewPager 实现一下
+3. 实现首页布局
+   1. 实现navBar
+   2. arrayAdapter 
+   3. okhttp 实现
+   
--- app/src/main/java/com/yuelin/interviewandroid/acitvitys/MainActivity.java
+++ app/src/main/java/com/yuelin/interviewandroid/acitvitys/MainActivity.java
@@ -1,8 +1,10 @@
-package com.yuelin.interviewandroid;
+package com.yuelin.interviewandroid.acitvitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.yuelin.interviewandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 

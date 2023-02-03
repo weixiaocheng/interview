a3f1ac1e8104d5c014f01857f42c279aeaa458a6
liuliang <454493675@qq.com>
2023-02-03 14:09:38 +0800

    先实现一个login 页面

--- app/src/main/java/com/yuelin/interviewandroid/acitvitys/WelcomeActivity.java
+++ app/src/main/java/com/yuelin/interviewandroid/acitvitys/WelcomeActivity.java
@@ -2,15 +2,48 @@ package com.yuelin.interviewandroid.acitvitys;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.yuelin.interviewandroid.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Timer timer;
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startLogin();
+            }
+        }, 2000);
+//        init();
+    }
+
+    private void init() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startLogin();
+            }
+        }, 1000);
+    }
+
+    private void startLogin() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

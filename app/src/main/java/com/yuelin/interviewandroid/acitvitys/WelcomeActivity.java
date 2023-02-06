339a8718949c30fce90e2e9d30fed12c467e2c10
liuliang <454493675@qq.com>
2023-02-06 10:01:44 +0800

    继续搞首页

--- app/src/main/java/com/yuelin/interviewandroid/acitvitys/WelcomeActivity.java
+++ app/src/main/java/com/yuelin/interviewandroid/acitvitys/WelcomeActivity.java
@@ -42,7 +42,7 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     private void startLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, TabbarActivity.class);
         startActivity(intent);
         finish();
     }

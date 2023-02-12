626427a32af607de395d226faea0758f90e6938e
liuliang <454493675@qq.com>
2023-02-12 11:37:36 +0800

    我都不知道后面的文字从哪里来了的

--- app/src/main/java/com/yuelin/interviewandroid/acitvitys/TabbarActivity.java
+++ app/src/main/java/com/yuelin/interviewandroid/acitvitys/TabbarActivity.java
@@ -1,5 +1,7 @@
 package com.yuelin.interviewandroid.acitvitys;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,6 +9,7 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -18,9 +21,17 @@ import com.yuelin.interviewandroid.components.TabbarItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TabbarActivity extends BaseActivity {
+public class TabbarActivity extends BaseActivity implements View.OnClickListener {
     private TabbarItem tabHome, tabNews, tabCollect, tabMine;
     private ViewPager viewPager;
+    private ArrayList<TabbarItem> tabbarList;
+
+    private int[] tabIds = {
+            R.id.home_tabbar_home,
+            R.id.home_tabbar_news,
+            R.id.home_tabbar_collect,
+            R.id.home_tabbar_mine};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +40,23 @@ public class TabbarActivity extends BaseActivity {
     }
 
     private void init() {
-        tabHome = (TabbarItem) findViewById(R.id.home_tabbar_home);
-        tabNews = (TabbarItem) findViewById(R.id.home_tabbar_news);
-        tabCollect = (TabbarItem) findViewById(R.id.home_tabbar_collect);
-        tabMine = (TabbarItem) findViewById(R.id.home_tabbar_mine);
-        tabHome.setSelected(true);
+        tabbarList = new ArrayList<TabbarItem>();
+        for (int index = 0; index < tabIds.length; index++) {
+            TabbarItem tabbarItem = findViewById(tabIds[index]);
+            tabbarList.add(tabbarItem);
+            if (index == 0) {
+                tabbarItem.setSelected(true);
+            }
+            tabbarItem.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.i(TAG, "onClick: " + view);
+        for (TabbarItem tabitem : tabbarList
+        ) {
+            tabitem.setSelected(view == tabitem);
+        }
     }
 }

657ed401bb55adb3b1da111d7705d7e2ef12dc88
liuliang <454493675@qq.com>
2023-02-14 11:07:15 +0800

    为方便调试先将获取新闻类目列表的调整到首页 , 然后重写网络封装

--- app/src/main/java/com/yuelin/interviewandroid/acitvitys/WelcomeActivity.java
+++ app/src/main/java/com/yuelin/interviewandroid/acitvitys/WelcomeActivity.java
@@ -41,7 +41,7 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     private void startLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }

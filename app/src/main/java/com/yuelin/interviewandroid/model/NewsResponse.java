4293acdd94a8fb18251f970717fdfe6d25c686e4
liuliang <454493675@qq.com>
2023-02-14 10:23:02 +0800

    添加了 新闻类目

--- app/src/main/java/com/yuelin/interviewandroid/model/NewsResponse.java
+++ app/src/main/java/com/yuelin/interviewandroid/model/NewsResponse.java
@@ -1,5 +1,25 @@
 package com.yuelin.interviewandroid.model;
 
-public class NewsResponse {
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
+public class NewsResponse {
+    public boolean isError;
+    public String message;
+    public int code;
+    public List<BeanNewItem> data;
+
+    public class BeanNewItem {
+        @SerializedName("id")
+        public int newsId;
+        public String title;
+        @SerializedName("sub_title")
+        public String subTitle;
+        @SerializedName("url_link")
+        public String urlLink;
+        @SerializedName("image_url")
+        public String imgUrl;
+    }
 }

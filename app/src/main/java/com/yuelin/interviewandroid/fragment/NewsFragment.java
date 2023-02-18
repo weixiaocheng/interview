863132602e7c5376b79c777a730abbbc311a797b
liuliang <454493675@qq.com>
2023-02-18 23:29:09 +0800

    在news 界面上添加了 grid 布局, 开始搞数据

--- app/src/main/java/com/yuelin/interviewandroid/fragment/NewsFragment.java
+++ app/src/main/java/com/yuelin/interviewandroid/fragment/NewsFragment.java
@@ -20,31 +20,10 @@ import java.util.List;
 
 public class NewsFragment extends Fragment {
 
-    private List<NewsResponse.BeanNewItem> listData;
-    public ListView listView;
-
-    // 使用单行列表 够用了
-    private RecyclerView.Adapter adapter;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_news, container, false);
     }
 
-
-    /**
-     * 设置list里面的数据
-     * @param list
-     */
-    public void setListData(List<NewsResponse.BeanNewItem> list) {
-        listData = list;
-        // 实现刷新逻辑
-        adapter.notifyDataSetChanged();
-    }
 }
